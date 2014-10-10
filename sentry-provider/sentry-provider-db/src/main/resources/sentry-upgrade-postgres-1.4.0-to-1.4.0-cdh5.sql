ad2f33a86be5cc6f6f2218bc8ede91fde15c98dd
Prasad Mujumdar <prasadm@cloudera.com>
2014-10-10 16:30:18 -0700

    CDH-22417: Backport SENTRY-496: Sentry postgres upgrade script has contains incorrect upgrade file name, quote table names

--- sentry-provider/sentry-provider-db/src/main/resources/sentry-upgrade-postgres-1.4.0-to-1.4.0-cdh5.sql
+++ sentry-provider/sentry-provider-db/src/main/resources/sentry-upgrade-postgres-1.4.0-to-1.4.0-cdh5.sql
@@ -3,5 +3,5 @@ SELECT 'Upgrading Sentry store schema from 1.4.0 to 1.4.0-cdh5';
 \i 002-SENTRY-339.postgres.sql;
 \i 003-SENTRY-380.postgres.sql;
 
-UPDATE SENTRY_VERSION SET SCHEMA_VERSION='1.4.0-cdh5', VERSION_COMMENT='Sentry release version 1.4.0-cdh5' WHERE VER_ID=1;
+UPDATE "SENTRY_VERSION" SET "SCHEMA_VERSION"='1.4.0-cdh5', "VERSION_COMMENT"='Sentry release version 1.4.0-cdh5' WHERE "VER_ID"=1;
 SELECT 'Finished upgrading Sentry store schema from 1.4.0 to 1.4.0-cdh5';
