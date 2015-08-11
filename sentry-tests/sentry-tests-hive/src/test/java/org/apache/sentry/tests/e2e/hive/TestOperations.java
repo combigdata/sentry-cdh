apache/sentry

SENTRY-810: CTAS without location is not verified properly (Ryan P via Lenni Kuff)

sentry-tests/sentry-tests-hive/src/test/java/org/apache/sentry/tests/e2e/hive/TestOperations.java
@@ -897,6 +897,8 @@ public class TestOperations extends AbstractTestWithStaticConfiguration {
     adminCreate(DB1, tableName);
     adminCreate(DB2, null);
 
+    String location = dfs.getBaseDir() + "/" + Math.random();
+
     Connection connection = context.createConnection(ADMIN1);
     Statement statement = context.createStatement(connection);
     statement.execute("Use " + DB1);
@@ -905,19 +907,27 @@ public class TestOperations extends AbstractTestWithStaticConfiguration {
     connection.close();
 
     policyFile
-        .addPermissionsToRole("select_db1_tb1", privileges.get("select_db1_tb1"))
-        .addPermissionsToRole("select_db1_view1", privileges.get("select_db1_view1"))
-        .addPermissionsToRole("create_db2", privileges.get("create_db2"))
-        .addRolesToGroup(USERGROUP1, "select_db1_tb1", "create_db2")
-        .addRolesToGroup(USERGROUP2, "select_db1_view1", "create_db2");
+      .addPermissionsToRole("select_db1_tb1", privileges.get("select_db1_tb1"))
+      .addPermissionsToRole("select_db1_view1", privileges.get("select_db1_view1"))
+      .addPermissionsToRole("create_db2", privileges.get("create_db2"))
+      .addPermissionsToRole("all_uri", "server=server1->uri=" + location)
+      .addRolesToGroup(USERGROUP1, "select_db1_tb1", "create_db2")
+      .addRolesToGroup(USERGROUP2, "select_db1_view1", "create_db2")
+      .addRolesToGroup(USERGROUP3, "select_db1_tb1", "create_db2,all_uri");
     writePolicyFile(policyFile);
 
     connection = context.createConnection(USER1_1);
     statement = context.createStatement(connection);
     statement.execute("Use " + DB2);
-    statement.execute("create table tb2 as select a from " + DB1 + ".tb1" );
+    statement.execute("create table tb2 as select a from " + DB1 + ".tb1");
+    //Ensure CTAS fails without URI
+    context.assertSentrySemanticException(statement, "create table tb3 location '" + location +
+        "' as select a from " + DB1 + ".tb1",
+      semanticException);
     context.assertSentrySemanticException(statement, "create table tb3 as select a from " + DB1 + ".view1",
-        semanticException);
+      semanticException);
+
+
     statement.close();
     connection.close();
 
@@ -926,11 +936,23 @@ public class TestOperations extends AbstractTestWithStaticConfiguration {
     statement.execute("Use " + DB2);
     statement.execute("create table tb3 as select a from " + DB1 + ".view1" );
     context.assertSentrySemanticException(statement, "create table tb4 as select a from " + DB1 + ".tb1",
-        semanticException);
+      semanticException);
 
     statement.close();
     connection.close();
+
+    connection = context.createConnection(USER3_1);
+    statement = context.createStatement(connection);
+    //CTAS is valid with URI
+    statement.execute("Use " + DB2);
+    statement.execute("create table tb4 location '" + location +
+      "' as select a from " + DB1 + ".tb1");
+
+    statement.close();
+    connection.close();
+
   }
+
 
   /*
   1. INSERT : IP: select on table, OP: insert on table + all on uri(optional)

combigdata/sentry-cdh

sentry-tests/sentry-tests-hive/src/test/java/org/apache/sentry/tests/e2e/hive/TestOperations.java
@@ -631,6 +631,8 @@ public class TestOperations extends AbstractTestWithStaticConfiguration {
     adminCreate(DB1, tableName);
     adminCreate(DB2, null);
 
+    String location = dfs.getBaseDir() + "/" + Math.random();
+
     Connection connection = context.createConnection(ADMIN1);
     Statement statement = context.createStatement(connection);
     statement.execute("Use " + DB1);
@@ -639,19 +641,27 @@ public class TestOperations extends AbstractTestWithStaticConfiguration {
     connection.close();
 
     policyFile
-        .addPermissionsToRole("select_db1_tb1", privileges.get("select_db1_tb1"))
-        .addPermissionsToRole("select_db1_view1", privileges.get("select_db1_view1"))
-        .addPermissionsToRole("all_db2", privileges.get("all_db2"))
-        .addRolesToGroup(USERGROUP1, "select_db1_tb1", "all_db2")
-        .addRolesToGroup(USERGROUP2, "select_db1_view1", "all_db2");
+      .addPermissionsToRole("select_db1_tb1", privileges.get("select_db1_tb1"))
+      .addPermissionsToRole("select_db1_view1", privileges.get("select_db1_view1"))
+      .addPermissionsToRole("all_db2", privileges.get("all_db2"))
+      .addPermissionsToRole("all_uri", "server=server1->uri=" + location)
+      .addRolesToGroup(USERGROUP1, "select_db1_tb1", "all_db2")
+      .addRolesToGroup(USERGROUP2, "select_db1_view1", "all_db2")
+      .addRolesToGroup(USERGROUP3, "select_db1_tb1", "all_db2,all_uri");
     writePolicyFile(policyFile);
 
     connection = context.createConnection(USER1_1);
     statement = context.createStatement(connection);
     statement.execute("Use " + DB2);
-    statement.execute("create table tb2 as select a from " + DB1 + ".tb1" );
+    statement.execute("create table tb2 as select a from " + DB1 + ".tb1");
+    //Ensure CTAS fails without URI
+    context.assertSentrySemanticException(statement, "create table tb3 location '" + location +
+        "' as select a from " + DB1 + ".tb1",
+      semanticException);
     context.assertSentrySemanticException(statement, "create table tb3 as select a from " + DB1 + ".view1",
-        semanticException);
+      semanticException);
+
+
     statement.close();
     connection.close();
 
@@ -660,11 +670,23 @@ public class TestOperations extends AbstractTestWithStaticConfiguration {
     statement.execute("Use " + DB2);
     statement.execute("create table tb3 as select a from " + DB1 + ".view1" );
     context.assertSentrySemanticException(statement, "create table tb4 as select a from " + DB1 + ".tb1",
-        semanticException);
+      semanticException);
 
     statement.close();
     connection.close();
+
+    connection = context.createConnection(USER3_1);
+    statement = context.createStatement(connection);
+    //CTAS is valid with URI
+    statement.execute("Use " + DB2);
+    statement.execute("create table tb4 location '" + location +
+      "' as select a from " + DB1 + ".tb1");
+
+    statement.close();
+    connection.close();
+
   }
+
 
   /*
   1. INSERT : IP: select on table, OP: insert on table + all on uri(optional)