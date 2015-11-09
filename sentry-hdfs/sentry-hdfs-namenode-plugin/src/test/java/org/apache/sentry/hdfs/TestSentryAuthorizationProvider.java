5436b3b5f16fb6be802c94a1794f3942e7c0d863
Sravya Tirukkovalur <sravya@cloudera.com>
2015-11-09 14:07:02 -0800

    SENTRY-944: Setting HDFS rules on Sentry managed hdfs paths should not affect original hdfs rules ( Hao Hao, Reviewed by: Sravya Tirukkovalur)
    Change-Id: Ia2c76360c157acaffe3d8b1351465bcf3501cb63
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/10016
    Tested-by: jenkins
    Reviewed-by: Hao Hao <hao.hao@cloudera.com>

--- sentry-hdfs/sentry-hdfs-namenode-plugin/src/test/java/org/apache/sentry/hdfs/TestSentryAuthorizationProvider.java
+++ sentry-hdfs/sentry-hdfs-namenode-plugin/src/test/java/org/apache/sentry/hdfs/TestSentryAuthorizationProvider.java
@@ -156,6 +156,60 @@ public class TestSentryAuthorizationProvider {
         Assert.assertEquals("supergroup", fs.getFileStatus(path).getGroup());
         Assert.assertEquals(new FsPermission((short) 0755), fs.getFileStatus(path).getPermission());
         Assert.assertTrue(fs.getAclStatus(path).getEntries().isEmpty());
+
+        //stale and dir inside of prefix, obj
+        System.setProperty("test.stale", "true");
+        path = new Path("/user/authz/xxx");
+        status = fs.getFileStatus(path);
+        Assert.assertEquals(sysUser, status.getOwner());
+        Assert.assertEquals("supergroup", status.getGroup());
+        Assert.assertEquals(new FsPermission((short) 0755), status.getPermission());
+        Assert.assertTrue(fs.getAclStatus(path).getEntries().isEmpty());
+
+        // setPermission sets the permission for dir outside of prefix.
+        // setUser/setGroup sets the user/group for dir outside of prefix.
+        Path pathOutside = new Path("/user/xxx");
+
+        fs.setPermission(pathOutside, new FsPermission((short) 0000));
+        Assert.assertEquals(new FsPermission((short) 0000), fs.getFileStatus(pathOutside).getPermission());
+        fs.setOwner(pathOutside, sysUser, "supergroup");
+        Assert.assertEquals(sysUser, fs.getFileStatus(pathOutside).getOwner());
+        Assert.assertEquals("supergroup", fs.getFileStatus(pathOutside).getGroup());
+
+        // removeAcl removes the ACL entries for dir outside of prefix.
+        List<AclEntry> aclsOutside = new ArrayList<AclEntry>(baseAclList);
+        List<AclEntry> acl = new ArrayList<AclEntry>();
+        acl.add(new AclEntry.Builder().setName("supergroup").setType(AclEntryType.GROUP).setScope(AclEntryScope.ACCESS).
+                setPermission(FsAction.READ_EXECUTE).build());
+        aclsOutside.addAll(acl);
+        fs.setAcl(pathOutside, aclsOutside);
+        fs.removeAclEntries(pathOutside, acl);
+        Assert.assertFalse(fs.getAclStatus(pathOutside).getEntries().containsAll(acl));
+
+        // setPermission sets the permission for dir inside of prefix but not a hive obj.
+        // setUser/setGroup sets the user/group for dir inside of prefix but not a hive obj.
+        Path pathInside = new Path("/user/authz/xxx");
+
+        fs.setPermission(pathInside, new FsPermission((short) 0000));
+        Assert.assertEquals(new FsPermission((short) 0000), fs.getFileStatus(pathInside).getPermission());
+        fs.setOwner(pathInside, sysUser, "supergroup");
+        Assert.assertEquals(sysUser, fs.getFileStatus(pathInside).getOwner());
+        Assert.assertEquals("supergroup", fs.getFileStatus(pathInside).getGroup());
+
+        // removeAcl is a no op for dir inside of prefix.
+        Assert.assertTrue(fs.getAclStatus(pathInside).getEntries().isEmpty());
+        fs.removeAclEntries(pathInside, acl);
+        Assert.assertTrue(fs.getAclStatus(pathInside).getEntries().isEmpty());
+
+        // setPermission/setUser/setGroup is a no op for dir inside of prefix, and is a hive obj.
+        Path pathInsideAndHive = new Path("/user/authz/obj");
+
+        fs.setPermission(pathInsideAndHive, new FsPermission((short) 0000));
+        Assert.assertEquals(new FsPermission((short) 0770), fs.getFileStatus(pathInsideAndHive).getPermission());
+        fs.setOwner(pathInsideAndHive, sysUser, "supergroup");
+        Assert.assertEquals("hive", fs.getFileStatus(pathInsideAndHive).getOwner());
+        Assert.assertEquals("hive", fs.getFileStatus(pathInsideAndHive).getGroup());
+
         return null;
       }
     });
