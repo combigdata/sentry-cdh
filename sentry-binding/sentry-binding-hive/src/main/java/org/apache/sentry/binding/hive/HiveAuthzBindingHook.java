0d95d08c3d50178f87caec4cf548086d1c76eb07
Sravya Tirukkovalur <sravya@apache.org>
2016-06-01 14:39:24 -0700

    SENTRY-1201: Sentry ignores database prefix for MSCK statement (Sravya Tirukkovalur, Reviewed by: Lenni Kuff)
    Change-Id: I79adbe505b3a469efdc83ecbda0682d4149de84e
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/13441
    Tested-by: Jenkins User
    Reviewed-by: Sravya Tirukkovalur <sravya@cloudera.com>

--- sentry-binding/sentry-binding-hive/src/main/java/org/apache/sentry/binding/hive/HiveAuthzBindingHook.java
+++ sentry-binding/sentry-binding-hive/src/main/java/org/apache/sentry/binding/hive/HiveAuthzBindingHook.java
@@ -28,6 +28,7 @@ import java.util.EnumSet;
 import java.util.List;
 import java.util.Set;
 
+import com.google.common.base.Preconditions;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hive.common.JavaUtils;
@@ -215,11 +216,7 @@ public class HiveAuthzBindingHook extends AbstractSemanticAnalyzerHook {
         currDB = extractDatabase((ASTNode)ast.getChild(0));
         break;
       case HiveParser.TOK_MSCK:
-        // token name TOK_TABNAME is not properly set in this case and child(0) does
-        // not contain the table name.
-        // TODO: Fix Hive to capture the table and DB name
-        currOutTab = extractTable((ASTNode)ast.getChild(1));
-        currOutDB  = extractDatabase((ASTNode)ast.getChild(0));
+        extractDbTableNameFromTOKTABLE((ASTNode) ast.getChild(1));
         break;
       case HiveParser.TOK_ALTERTABLE_ADDPARTS:
         /*
@@ -278,6 +275,14 @@ public class HiveAuthzBindingHook extends AbstractSemanticAnalyzerHook {
     return new Database(SessionState.get().getCurrentDatabase());
   }
 
+  protected void extractDbTableNameFromTOKTABLE(ASTNode astNode) throws SemanticException{
+    String[] fqTableName = BaseSemanticAnalyzer.getQualifiedTableName(astNode);
+    Preconditions.checkArgument(fqTableName.length == 2, "BaseSemanticAnalyzer.getQualifiedTableName should return " +
+            "an array with dbName and tableName");
+    currOutDB = new Database(fqTableName[0]);
+    currOutTab = new Table(fqTableName[1]);
+  }
+
   private Database extractDatabase(ASTNode ast) throws SemanticException {
     String tableName = BaseSemanticAnalyzer.getUnescapedName(ast);
     if (tableName.contains(".")) {
@@ -286,6 +291,7 @@ public class HiveAuthzBindingHook extends AbstractSemanticAnalyzerHook {
       return getCanonicalDb();
     }
   }
+  /*TODO: Deprecate */
   private Table extractTable(ASTNode ast) throws SemanticException {
     String tableName = BaseSemanticAnalyzer.getUnescapedName(ast);
     if (tableName.contains(".")) {
