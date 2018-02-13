df54ea28592aeeb8dfebb74d17254c202efbc59b
Carl Myers <cmyers@cloudera.com>
2018-02-13 09:58:09 -0800

    RELENG-3084 - modify precommit hook to use mvn-gbn wrapper
    Change-Id: Ib69f7ba5288b0aee9c59b7342e8463085af22c76

--- cloudera/pre_commit_hook.sh
+++ cloudera/pre_commit_hook.sh
@@ -9,12 +9,11 @@ source /opt/toolchain/toolchain.sh
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# get the CDH_GBN
-export BUILD_BRANCH=${GERRIT_BRANCH}
-source $DIR/setup_build_env.sh
+# activate mvn-gbn wrapper
+mv "$(which mvn-gbn-wrapper)" "$(dirname "$(which mvn-gbn-wrapper)")/mvn"
 
 # clean the old test class compiles
 find . -name test-classes | grep target/test-classes | xargs rm -rf
 
 # execute all tests (disable slow tests for now)
-mvn -s "${BUILD_SETTINGS_FILE}" test -PskipOnlySlowTests --fail-at-end
+mvn test -PskipOnlySlowTests --fail-at-end
