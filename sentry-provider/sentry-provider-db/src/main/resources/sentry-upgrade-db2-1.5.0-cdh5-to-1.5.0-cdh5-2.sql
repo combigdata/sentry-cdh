87c8693ea2a6f594af4d4e8a48c30f53d44d61ca
lina.li <lina.li@cloudera.com>
2018-05-01 12:39:51 -0500

    CDH-67922: Port to CDH5 SENTRY-2210: AUTHZ_PATH should have index on the foreign key AUTHZ_OBJ_ID
    ==C5_APPROVED_BUGFIX==
    
    Change-Id: Ie6e532449cdbc2c744dc3abe0c297aff9cdc71bd
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/35025
    Tested-by: Jenkins User <jenkins@cloudera.com>
    Reviewed-by: Sergio Pena <sergio.pena@cloudera.com>

--- sentry-provider/sentry-provider-db/src/main/resources/sentry-upgrade-db2-1.5.0-cdh5-to-1.5.0-cdh5-2.sql
+++ sentry-provider/sentry-provider-db/src/main/resources/sentry-upgrade-db2-1.5.0-cdh5-to-1.5.0-cdh5-2.sql
@@ -1,2 +1,5 @@
+-- create index for foreign key AUTHZ_OBJ_ID
+CREATE INDEX AUTHZ_PATH_FK_IDX ON AUTHZ_PATH (AUTHZ_OBJ_ID);
+
 -- Version update
 UPDATE SENTRY_VERSION SET SCHEMA_VERSION='1.5.0-cdh5-2', VERSION_COMMENT='Sentry release version 1.5.0-cdh5-2' WHERE VER_ID=1;
