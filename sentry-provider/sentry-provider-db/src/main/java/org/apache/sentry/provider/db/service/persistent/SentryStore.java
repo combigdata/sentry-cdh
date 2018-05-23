bbd55e79e350d58f2f20614de979a28975e06748
lina.li <lina.li@cloudera.com>
2018-05-23 14:49:35 -0500

    CDH-68729 for 6.0.x SENTRY-2144: Table Rename Cross Database should update permission correctly
    Change-Id: Ib82699f59bac8db4dce163c111659e58ec820c06

--- sentry-provider/sentry-provider-db/src/main/java/org/apache/sentry/provider/db/service/persistent/SentryStore.java
+++ sentry-provider/sentry-provider-db/src/main/java/org/apache/sentry/provider/db/service/persistent/SentryStore.java
@@ -2428,6 +2428,8 @@ public class SentryStore {
         if (newTPrivilege.getPrivilegeScope().equals(PrivilegeScope.DATABASE.name())) {
           tPriv.setDbName(newTPrivilege.getDbName());
         } else if (newTPrivilege.getPrivilegeScope().equals(PrivilegeScope.TABLE.name())) {
+          // the DB name could change, so set its value
+          tPriv.setDbName(newTPrivilege.getDbName());
           tPriv.setTableName(newTPrivilege.getTableName());
         }
         alterSentryRoleGrantPrivilegeCore(pm, role.getRoleName(), tPriv);
