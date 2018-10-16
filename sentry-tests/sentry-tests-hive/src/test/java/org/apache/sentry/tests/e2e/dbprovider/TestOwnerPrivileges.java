1db8ca68848a9b11e7608c49e16848eabe6ce0dc
lina.li <lina.li@cloudera.com>
2018-10-16 15:49:33 -0500

    CDH-74232: SENTRY-2429: Transfer database owner drops table owner
    ==C5_FEATURE_FGP==
    
    Change-Id: Ic9cbea7741f9b6cd4dd66941953da14d488fcf6c
    Reviewed-on: https://gerrit.sjc.cloudera.com/38147
    Tested-by: Jenkins User <jenkins@cloudera.com>
    Reviewed-by: Na Li <lina.li@cloudera.com>

--- sentry-tests/sentry-tests-hive/src/test/java/org/apache/sentry/tests/e2e/dbprovider/TestOwnerPrivileges.java
+++ sentry-tests/sentry-tests-hive/src/test/java/org/apache/sentry/tests/e2e/dbprovider/TestOwnerPrivileges.java
@@ -762,6 +762,145 @@ public class TestOwnerPrivileges extends TestHDFSIntegrationBase {
   }
 
   /**
+   * Verify that if the same user is owner of both DB and table, after alter DB's owner,
+   * the table owner is still that user
+   *
+   * @throws Exception
+   */
+  @Test
+  public void testAlterDBNotDropTableOwnerSameOwner() throws Exception {
+    String allWithGrantRole = "allWithGrant_role";
+    String ownerRole = "owner_role";
+    dbNames = new String[]{DB1};
+    roles = new String[]{"admin_role", "create_db1", "owner_role"};
+
+    // create required roles
+    setupUserRoles(roles, statement);
+
+    // remove test DB if it exists
+    statement.execute("DROP DATABASE IF EXISTS " + DB1 + " CASCADE");
+
+    // setup privileges for USER1
+    statement.execute("GRANT CREATE ON SERVER server1 TO ROLE create_db1");
+
+    // USER1 creates test DB
+    Connection connectionUSER1_1 = hiveServer2.createConnection(USER1_1, USER1_1);
+    Statement statementUSER1_1 = connectionUSER1_1.createStatement();
+    statementUSER1_1.execute("CREATE DATABASE " + DB1);
+    statementUSER1_1.execute("USE " + DB1);
+
+    // USER1 create table
+    statementUSER1_1.execute("CREATE TABLE " + DB1 + "." + tableName1
+        + " (under_col int comment 'the under column')");
+
+    // verify privileges created for new database
+    verifyTableOwnerPrivilegeExistForPrincipal(statementUSER1_1, SentryPrincipalType.USER, Lists.newArrayList(USER1_1),
+        DB1, "", 1);
+
+    // verify privileges created for new table
+    verifyTableOwnerPrivilegeExistForPrincipal(statementUSER1_1, SentryPrincipalType.USER, Lists.newArrayList(USER1_1),
+        DB1, tableName1, 1);
+
+    // change db owner
+    // setup all privilege for USERGROUP2
+    statement.execute("create role " + allWithGrantRole);
+    statement.execute("grant role " + allWithGrantRole + " to group " + USERGROUP2);
+    statement.execute("GRANT ALL ON DATABASE " + DB1 + " to role " +
+        allWithGrantRole + " with grant option");
+    Connection connectionUSER2_1 = hiveServer2.createConnection(USER2_1, USER2_1);
+    Statement statementUSER2_1 = connectionUSER2_1.createStatement();
+    statementUSER2_1.execute("ALTER DATABASE " + DB1 + " SET OWNER ROLE " + "owner_role");
+
+    // Verify that new owner has owner privilege on DB
+    verifyTableOwnerPrivilegeExistForPrincipal(statement, SentryPrincipalType.ROLE,
+        Lists.newArrayList(ownerRole), DB1, "", 1);
+
+    // Verify table still has its owner
+    verifyTableOwnerPrivilegeExistForPrincipal(statementUSER1_1, SentryPrincipalType.USER, Lists.newArrayList(USER1_1),
+          DB1, tableName1, 1);
+
+    statement.execute("DROP ROLE " + allWithGrantRole);
+
+    statement.close();
+    connection.close();
+
+    statementUSER1_1.close();
+    connectionUSER1_1.close();
+
+    statementUSER2_1.close();
+    connectionUSER2_1.close();
+  }
+
+  /**
+   * Verify that if owner of DB is different from owner of its table, after alter DB's owner,
+   * the table owner still exists
+   *
+   * @throws Exception
+   */
+  @Test
+  public void testAlterDBNotDropTableOwnerDifferentOwner() throws Exception {
+    String allWithGrantRole = "allWithGrant_role";
+    String ownerRole = "owner_role";
+    dbNames = new String[]{DB1};
+    roles = new String[]{"admin_role", "create_db1", "owner_role"};
+
+    // create required roles
+    setupUserRoles(roles, statement);
+
+    // remove test DB if it exists, then create the DB, so its owner is admin
+    statement.execute("DROP DATABASE IF EXISTS " + DB1 + " CASCADE");
+    statement.execute("CREATE DATABASE " + DB1);
+
+    // setup privileges for USER1
+    statement.execute("GRANT CREATE ON SERVER server1 TO ROLE create_db1");
+    Connection connectionUSER1_1 = hiveServer2.createConnection(USER1_1, USER1_1);
+    Statement statementUSER1_1 = connectionUSER1_1.createStatement();
+    statementUSER1_1.execute("USE " + DB1);
+
+    // USER1 create table and becomes owner of that table
+    statementUSER1_1.execute("CREATE TABLE " + DB1 + "." + tableName1
+        + " (under_col int comment 'the under column')");
+
+    // verify privileges created for new database
+    verifyTableOwnerPrivilegeExistForPrincipal(statement, SentryPrincipalType.USER, Lists.newArrayList(admin),
+        DB1, "", 1);
+
+    // verify privileges created for new table
+    verifyTableOwnerPrivilegeExistForPrincipal(statementUSER1_1, SentryPrincipalType.USER, Lists.newArrayList(USER1_1),
+        DB1, tableName1, 1);
+
+    // change db owner
+    // setup all privilege for USERGROUP2
+    statement.execute("create role " + allWithGrantRole);
+    statement.execute("grant role " + allWithGrantRole + " to group " + USERGROUP2);
+    statement.execute("GRANT ALL ON DATABASE " + DB1 + " to role " +
+        allWithGrantRole + " with grant option");
+    Connection connectionUSER2_1 = hiveServer2.createConnection(USER2_1, USER2_1);
+    Statement statementUSER2_1 = connectionUSER2_1.createStatement();
+    statementUSER2_1.execute("ALTER DATABASE " + DB1 + " SET OWNER ROLE " + "owner_role");
+
+    // Verify that new owner has owner privilege on DB
+    verifyTableOwnerPrivilegeExistForPrincipal(statement, SentryPrincipalType.ROLE,
+        Lists.newArrayList(ownerRole), DB1, "", 1);
+
+    // Verify table still has its owner
+    verifyTableOwnerPrivilegeExistForPrincipal(statementUSER1_1, SentryPrincipalType.USER, Lists.newArrayList(USER1_1),
+        DB1, tableName1, 1);
+
+    statement.execute("DROP ROLE " + allWithGrantRole);
+
+    statement.close();
+    connection.close();
+
+    statementUSER1_1.close();
+    connectionUSER1_1.close();
+
+    statementUSER2_1.close();
+    connectionUSER2_1.close();
+  }
+
+
+  /**
    * Verify that the user who can call alter table set owner on this table
    *
    * @throws Exception
