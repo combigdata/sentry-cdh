c1e2ae5e949be2f7ebcca00c265f9c76d39af742
lina.li <lina.li@cloudera.com>
2019-02-28 06:56:05 -0600

    CDH-78132: Sentry compilation failure in cdh6.2.x build
    Change-Id: Id7fbe985f4b28033b1764c7d138a9e965413d730

--- sentry-service/sentry-service-server/src/main/java/org/apache/sentry/provider/db/service/persistent/QueryParamBuilder.java
+++ sentry-service/sentry-service-server/src/main/java/org/apache/sentry/provider/db/service/persistent/QueryParamBuilder.java
@@ -31,6 +31,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Collection;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
